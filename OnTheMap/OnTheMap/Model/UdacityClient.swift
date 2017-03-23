939d8f6d6b1c3abbad47a8daef76ff4070742f9c
Jacob Taxén <jacob.taxen@afconsult.com>
2017-03-23 11:52:34 +0100

    API Request task for Udacity created. TODO: Must parse data

--- OnTheMap/OnTheMap/Model/UdacityClient.swift
+++ OnTheMap/OnTheMap/Model/UdacityClient.swift
@@ -12,21 +12,48 @@ class UdacityClient: NSObject {
 	
 	
 	
-	
-	// MARK: Create Udacity request URL
-	private func createUdacityURL(_ parameters: [String: AnyObject], method: String, withStringExtension: String? = nil, completionHandler: @escaping (_ succes: Bool, _ results: String?, _ error: String?) -> Void ) {
-	
-		var components = URLComponents()
-		components.scheme = Constants.Scheme
-		components.host = Constants.Host
-		components.path = Constants.Path + (withStringExtension ?? "")
-		components.queryItems = [URLQueryItem]()
+	func taskForPOST(parameters: [String: AnyObject], jsonBody: String, completionHandler: @escaping (_ results: AnyObject?, _ error: String?) -> Void ) {
 		
-		for (key, value) in parameters {
-			let item = URLQueryItem(name: key, value: "\(value)")
-			components.queryItems!.append(item)
+		/* 1. Set parameters */
+		let username = parameters[ParameterKeys.Username]
+		let password = parameters[ParameterKeys.Password]
+		let httpBody = "{\"udacity\": {\"username\": \"\(username)\", \" password\": \"\(password)\"}}"
+		
+		/* 2. Build URL */
+		let url = URL(string: Constants.Scheme + "://" + Constants.Host + Constants.Path)
+		let request = NSMutableURLRequest(url: url!)
+		
+		/* 3. Configure request */
+		request.httpMethod = "POST"
+		request.addValue("application/json", forHTTPHeaderField: "Accept")
+		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+		request.httpBody = httpBody.data(using: String.Encoding.utf8)
+		
+		/* 4. Make request */
+		let session = URLSession.shared
+		let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+
+			guard error != nil else {
+				print("There was an error")
+				return
+			}
+			
+			guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+				print("Server returned status code other than 2xx")
+				return
+			}
+			
+			guard let data = data else {
+				print("No data was returned")
+				return
+			}
+		
+		/* 5/6. Parse data */
+			
 		}
-		
+		/* 7. Start request */
+		task.resume()
+		return task
 	}
 	
 }
--- OnTheMap/OnTheMap/Model/UdacityClientConvenience.swift
+++ OnTheMap/OnTheMap/Model/UdacityClientConvenience.swift
@@ -10,7 +10,17 @@ import Foundation
 
 extension UdacityClient {
 
-	private func getSessionID()
+	private func getSessionID(username: String, password: String , completionHandler: @escaping (_ success: Bool, _ result: String?, _ error: String?) -> Void ){
+		
+		let udacityPair = [username: password]
+		let parameters = [ParameterKeys.Username: username,
+		                  ParameterKeys.Password: password]
+		
+		let _ = 
+		
+		
+		
+	}
 
 
 
