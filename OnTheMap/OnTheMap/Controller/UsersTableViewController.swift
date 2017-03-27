2848db6f79afd411e9c32e78fab6392d29b6b890
Jacob Taxén <jacob.taxen@afconsult.com>
2017-03-27 17:07:32 +0200

    TableView cells configured and open web pages when clicked on.

--- OnTheMap/OnTheMap/Controller/UsersTableViewController.swift
+++ OnTheMap/OnTheMap/Controller/UsersTableViewController.swift
@@ -40,60 +40,31 @@ class UsersTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! UserTableViewCell
 		
 		let entry = appDelegate.locationData[indexPath.row]
 		
-		cell.textLabel?.text = "\(entry["firstName"]?) \(entry["lastName"]?)"
-		cell.detailTextLabel?.text = "\(entry["mediaURL"]?)"
-		cell.imageView?.image = UIImage(named: "icon_pin")
-
+		let firstName = entry["firstName"] as? String ?? ""
+		let lastName = entry["lastName"] as? String ?? ""
+		let mediaUrl = entry["mediaURL"] as? String ?? ""
+		
+		cell.textLabel?.text = firstName + " " + lastName
+		cell.detailTextLabel?.text = mediaUrl
+		
         return cell
-    }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let cell = tableView.cellForRow(at: indexPath)
+		
+		if let url = URL(string: (cell?.detailTextLabel?.text)!) {
+			UIApplication.shared.open(url, options: [:], completionHandler: nil)
+		} else {
+			let alert = UIAlertController(title: "Invalid link", message: "Web page could not be opened", preferredStyle: .alert)
+			alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+			present(alert, animated: true, completion: nil)
+		}
+		
+	}
 
 }
