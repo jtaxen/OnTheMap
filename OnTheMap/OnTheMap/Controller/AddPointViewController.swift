9a5dc820005621b213bb7697698d5f4bdb3ce339
Jacob Taxén <jacob.taxen@afconsult.com>
2017-03-28 10:29:57 +0200

    Add point

---
+++ OnTheMap/OnTheMap/Controller/AddPointViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddPointViewController.swift
+//  OnTheMap
+//
+//  Created by ÅF Jacob Taxén on 2017-03-28.
+//  Copyright © 2017 Jacob Taxén. All rights reserved.
+//
+
+import UIKit
+
+class AddPointViewController: UIViewController {
+
+	@IBOutlet weak var textFieldContainer: UIView!
+	@IBOutlet weak var locationField: UITextField!
+	@IBOutlet weak var websiteField: UITextField!
+	
+	
+	
+}
